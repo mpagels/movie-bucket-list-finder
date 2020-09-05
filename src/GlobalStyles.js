e447bb6f4efc10fca7ddbee192b31f074b98524d
Martin <office@martinpagels.de>
2020-09-05 21:36:02 +0200

    Add Global styles

---
+++ src/GlobalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components/macro";
+
+export default createGlobalStyle`
+* {
+    box-sizing: border-box;
+    font-family: "Open Sans", sans-serif;
+}
+
+body {
+    color: #0F1C2E;
+    margin: 20px;
+    font-size: 112.5%;
+
+}`;
