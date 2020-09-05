972758bf0c5d96be4aed12793ca89e1c5cfce6f1
Martin <office@martinpagels.de>
2020-09-05 21:45:51 +0200

    Add movie bucket finder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import buckelist from "./bucketList_20200508.json";
+import styled from "styled-components";
+
+const allMovies = Object.keys(buckelist[0]);
 
 function App() {
-  return <div>Hello World</div>;
+  const [filterdMovies, setFilterdMovies] = useState(allMovies);
+
+  return (
+    <Wrapper>
+      <Input
+        placeholder="Movie Name"
+        type="text"
+        onChange={inputHandler}
+      ></Input>
+      {filterdMovies.map((movieName, index) => (
+        <p key={index}>{movieName}</p>
+      ))}
+    </Wrapper>
+  );
+
+  function inputHandler(event) {
+    setFilterdMovies(
+      allMovies.filter((movieTitle) =>
+        movieTitle.toLowerCase().includes(event.target.value.toLowerCase())
+      )
+    );
+  }
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  p {
+    font-size: 24px;
+    text-align: center;
+  }
+`;
+
+const Input = styled.input`
+  all: unset;
+  border-bottom: 1px solid #d1def0;
+  border-left: none;
+  border-right: none;
+  border-top: none;
+  color: grey;
+  font-size: 38px;
+  margin: 20px;
+  padding: 0 20px 0 20px;
+  text-align: center;
+`;
