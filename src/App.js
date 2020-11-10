c9bd6e97c3627060698fe99db0b85d1fe68631a2
Martin Pagels <54703643+mpagels@users.noreply.github.com>
2020-11-10 23:02:28 +0100

    Hook/useMovieFilter (#3)
    * Add useMovieFilter hook
    
    * Refactor App.js with Input
    
    * Refactor FilterButton

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import buckelist from './bucketList_20200508.json'
 import styled from 'styled-components'
 import Movie from './Movie'
 import FilterButton from './components/FilterButton/FilterButton'
-
-const allMovies = Object.keys(buckelist[0])
+import useFilterMovies from './hooks/useMovieFilter'
+import InputField from './components/Inputs/InputField'
 
 function App() {
-  const [filterdMovies, setFilterdMovies] = useState(allMovies)
+  const [filterdMovies, filterMovies] = useFilterMovies()
   const [isActive, setIsActive] = useState('')
   const isEmpty = filterdMovies.length === 0
   const [activeFilter, setActiveFilter] = useState('movie')
@@ -15,29 +15,27 @@ function App() {
 
   return (
     <Wrapper>
-      <Input
+      <InputField
         placeholder="Search List by"
         type="text"
-        onChange={(event) => inputHandler(event, activeFilter)}
-      ></Input>
+        activeFilter={activeFilter}
+        onChange={inputHandler}
+      />
       <FilterWrapper>
         <FilterButton
           title="movie"
-          isActive={activeFilter === 'movie'}
+          activeFilter={activeFilter}
           onClick={handleFilter}
-          filterName="movie"
         />
         <FilterButton
           title="actor"
-          isActive={activeFilter === 'actor'}
+          activeFilter={activeFilter}
           onClick={handleFilter}
-          filterName="actor"
         />
         <FilterButton
           title="director"
-          isActive={activeFilter === 'director'}
+          activeFilter={activeFilter}
           onClick={handleFilter}
-          filterName="director"
         />
       </FilterWrapper>
 
@@ -63,7 +61,7 @@ function App() {
   function inputHandler(event, filter) {
     setIsActive('')
     setSearchInput(event.target.value)
-    filterMovies(filter)
+    filterMovies(filter, event.target.value)
   }
 
   function handleClick(index) {
@@ -72,33 +70,7 @@ function App() {
 
   function handleFilter(filter) {
     setActiveFilter(filter)
-    filterMovies(filter)
-  }
-
-  function filterMovies(filter) {
-    filter === 'movie'
-      ? setFilterdMovies(
-          allMovies.filter((movieTitle) =>
-            movieTitle.toLowerCase().includes(searchInput.toLowerCase())
-          )
-        )
-      : filter === 'actor'
-      ? setFilterdMovies(
-          allMovies.filter(
-            (movieTitle) =>
-              buckelist[0][movieTitle].tmdb.actors.filter((name) =>
-                name.toLowerCase().includes(searchInput.toLowerCase())
-              ).length > 0
-          )
-        )
-      : setFilterdMovies(
-          allMovies.filter(
-            (movieTitle) =>
-              buckelist[0][movieTitle].tmdb.director.filter((name) =>
-                name.toLowerCase().includes(searchInput.toLowerCase())
-              ).length > 0
-          )
-        )
+    filterMovies(filter, searchInput)
   }
 }
 
@@ -110,23 +82,8 @@ const Wrapper = styled.div`
   justify-content: center;
 `
 
-const Input = styled.input`
-  all: unset;
-  border-bottom: 1px solid #d1def0;
-  border-left: none;
-  border-right: none;
-  border-top: none;
-  color: #14253e;
-  font-size: 38px;
-  margin: 20px;
-  padding: 0 20px 0 20px;
-  text-align: center;
-  &::placeholder {
-    color: #1e375c;
-  }
-`
-
 const Alert = styled.p`
+  text-align: center;
   color: #1e375c;
 `
 
---
+++ src/hooks/useMovieFilter.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import buckelist from '../bucketList_20200508.json'
+const allMovies = Object.keys(buckelist[0])
+
+export default function useMovieFilter() {
+  const [filterdMovies, setFilterdMovies] = useState(allMovies)
+
+  function filterMovies(filter, searchInput) {
+    filter === 'movie'
+      ? setFilterdMovies(
+          allMovies.filter((movieTitle) =>
+            movieTitle.toLowerCase().includes(searchInput.toLowerCase())
+          )
+        )
+      : filter === 'actor'
+      ? setFilterdMovies(
+          allMovies.filter(
+            (movieTitle) =>
+              buckelist[0][movieTitle].tmdb.actors.filter((name) =>
+                name.toLowerCase().includes(searchInput.toLowerCase())
+              ).length > 0
+          )
+        )
+      : setFilterdMovies(
+          allMovies.filter(
+            (movieTitle) =>
+              buckelist[0][movieTitle].tmdb.director.filter((name) =>
+                name.toLowerCase().includes(searchInput.toLowerCase())
+              ).length > 0
+          )
+        )
+  }
+
+  return [filterdMovies, filterMovies]
+}
