c9fb982d5ebc0202389faad13fdb78677178b75a
Martin Pagels <office@martinpagels.de>
2021-01-24 00:52:14 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 import buckelist from './bucketList_20200508.json'
 import styled from 'styled-components'
 import Movie from './Movie'
@@ -7,12 +7,21 @@ import useFilterMovies from './hooks/useMovieFilter'
 import InputField from './components/Inputs/InputField'
 
 function App() {
-  const [filterdMovies, filterMovies] = useFilterMovies()
+  const {
+    filterdMovies,
+    filterMovies,
+    activeGenreFilter,
+    setActiveGenreFilter,
+  } = useFilterMovies()
+
   const [isActive, setIsActive] = useState('')
   const isEmpty = filterdMovies.length === 0
   const [activeFilter, setActiveFilter] = useState('movie')
   const [searchInput, setSearchInput] = useState('')
+  const [genreIsOpen, setGenreIsOpen] = useState(false)
+  const genresList = useMemo(() => createGenres(buckelist), [buckelist])
 
+  console.log(genresList)
   return (
     <Wrapper>
       <InputField
@@ -38,7 +47,22 @@ function App() {
           onClick={handleFilter}
         />
       </FilterWrapper>
-
+      <GenreButton onClick={handleGenreButton} isActive={genreIsOpen}>
+        genre
+      </GenreButton>
+      {genreIsOpen && (
+        <GenreWrapper>
+          {genresList.map((genre, index) => (
+            <MiniButton
+              key={genre}
+              isActive={genre === activeGenreFilter}
+              onClick={() => handleOnGenrePick(genre)}
+            >
+              {genre}
+            </MiniButton>
+          ))}
+        </GenreWrapper>
+      )}
       {isEmpty ? (
         <Alert>
           Movie not in list <br />
@@ -72,6 +96,24 @@ function App() {
     setActiveFilter(filter)
     filterMovies(filter, searchInput)
   }
+
+  function handleGenreButton() {
+    setGenreIsOpen((state) => !state)
+  }
+
+  function createGenres(movielist) {
+    const movieList = Object.keys(movielist[0])
+    const genres = movieList.map((movie) => {
+      const genres = movielist[0][movie].tmdb.genres.map((genre) => genre.name)
+      return [...genres]
+    })
+    const uniqueGenres = new Set(genres.flat())
+    return Array.from(uniqueGenres)
+  }
+
+  function handleOnGenrePick(genre) {
+    setActiveGenreFilter(genre !== activeGenreFilter ? genre : '')
+  }
 }
 
 export default App
@@ -90,4 +132,39 @@ const Alert = styled.p`
 const FilterWrapper = styled.div`
   display: flex;
   justify-content: space-around;
+`
+
+const GenreButton = styled.button`
+  all: unset;
+  text-align: center;
+  padding: 3px;
+  margin: 10px;
+  border-radius: 15px;
+  ${(props) =>
+    props.isActive
+      ? 'background-color: var(--ButtonActive); color: white;'
+      : 'background-color: white; color: var(--ButtonActive);'}
+  border: solid 2px var(--ButtonActive);
+  cursor: pointer;
+`
+
+const MiniButton = styled.button`
+  all: unset;
+  text-align: center;
+  padding: 3px;
+  margin: 3px;
+  border-radius: 2px;
+  ${(props) =>
+    props.isActive
+      ? 'background-color: var(--ButtonActive); color: white;'
+      : 'background-color: white; color: var(--ButtonActive);'}
+  border: solid 2px var(--ButtonActive);
+  cursor: pointer;
+`
+
+const GenreWrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100vw;
 `
--- src/hooks/useMovieFilter.js
+++ src/hooks/useMovieFilter.js
@@ -4,32 +4,62 @@ const allMovies = Object.keys(buckelist[0])
 
 export default function useMovieFilter() {
   const [filterdMovies, setFilterdMovies] = useState(allMovies)
+  const [activeGenreFilter, setActiveGenreFilter] = useState('')
+  console.log(activeGenreFilter)
 
   function filterMovies(filter, searchInput) {
-    filter === 'movie'
-      ? setFilterdMovies(
-          allMovies.filter((movieTitle) =>
-            movieTitle.toLowerCase().includes(searchInput.toLowerCase())
-          )
-        )
-      : filter === 'actor'
-      ? setFilterdMovies(
-          allMovies.filter(
-            (movieTitle) =>
-              buckelist[0][movieTitle].tmdb.actors.filter((name) =>
-                name.toLowerCase().includes(searchInput.toLowerCase())
-              ).length > 0
-          )
-        )
-      : setFilterdMovies(
-          allMovies.filter(
-            (movieTitle) =>
-              buckelist[0][movieTitle].tmdb.director.filter((name) =>
-                name.toLowerCase().includes(searchInput.toLowerCase())
-              ).length > 0
-          )
-        )
+    const moviesToSearch =
+      activeGenreFilter !== ''
+        ? allMovies.filter((movie) => {
+            const genres = buckelist[0][movie].tmdb.genres.filter(
+              (genre) => genre.name === activeGenreFilter
+            )
+            return genres.length > 0
+          })
+        : allMovies
+
+    const movie =
+      filter === 'movie'
+        ? movieSearch(moviesToSearch, searchInput)
+        : filter === 'actor'
+        ? actorSearch(moviesToSearch, searchInput)
+        : directorSearch(moviesToSearch, searchInput)
+
+    setFilterdMovies(movie)
   }
 
-  return [filterdMovies, filterMovies]
+  return {
+    filterdMovies,
+    filterMovies,
+    activeGenreFilter,
+    setActiveGenreFilter,
+  }
+
+  function movieSearch(movieList, searchInput) {
+    const movies = movieList.filter((movieTitle) =>
+      movieTitle.toLowerCase().includes(searchInput.toLowerCase())
+    )
+    return movies
+  }
+
+  function actorSearch(movieList, searchInput) {
+    const movies = movieList.filter(
+      (movieTitle) =>
+        buckelist[0][movieTitle].tmdb.actors.filter((name) =>
+          name.toLowerCase().includes(searchInput.toLowerCase())
+        ).length > 0
+    )
+    return movies
+  }
+
+  function directorSearch(movieList, searchInput) {
+    const movies = movieList.filter(
+      (movieTitle) =>
+        buckelist[0][movieTitle].tmdb.director.filter((name) =>
+          name.toLowerCase().includes(searchInput.toLowerCase())
+        ).length > 0
+    )
+
+    return movies
+  }
 }
